9a12ed26e9bcc91521aee864b8f8181eef714fb6
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-06-11 23:34:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+ var customerName = "bob";
+const leastFavoriteCustomer = "unsuccessfully tries to reassign the least favorite customer";
+ function upperCaseCustomerName(){
+customerName = customerName.toUpperCase();
+ }
+
+ function setBestCustomer(){
+    bestCustomer = "not bob";
+ }
+ function overwriteBestCustomer(){
+     bestCustomer = "maybe bob";
+ }
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Assignment to constant variable.";
+ }
